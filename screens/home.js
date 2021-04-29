47acd198f85821d1652188f55dd51060fb075e20
voquangluu997 <voquangluu997@gmail.com>
2021-04-29 18:41:05 +0700

    init home screen

---
+++ screens/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { globalStyles } from "../styles/global";
+import { Formik } from "formik";
+import { FlatButton, TransParentButton } from "../shared/button";
+
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
